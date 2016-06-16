a6c0e866618286caae83df506f2e7dda1e6e81db
471754624@qq.com <471754624@qq.com>
2016-06-16 12:02:55 +0800

    bb2

--- OOB/OOB/ViewController.m
+++ OOB/OOB/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"0.0.viewDidLoad");
+     NSLog(@"0.0.viewDidLoad2");
 }
 
 - (void)didReceiveMemoryWarning {
