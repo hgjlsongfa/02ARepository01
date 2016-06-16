7ef6868b0a9c1f235672da85256b5f587af539bd
471754624@qq.com <471754624@qq.com>
2016-06-16 11:25:38 +0800

    01

---
+++ adfadsf/adfadsf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adfadsf
+//
+//  Created by lisongfa on 16/6/16.
+//  Copyright © 2016年 lisongfa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
