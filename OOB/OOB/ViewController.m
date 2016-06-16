9cc61921bec60421a7ff213a671b58d0a9da1dfa
471754624@qq.com <471754624@qq.com>
2016-06-16 12:00:41 +0800

    BB

---
+++ OOB/OOB/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  OOB
+//
+//  Created by lisongfa on 16/6/16.
+//  Copyright © 2016年 lisongfa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"0.0.viewDidLoad");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
